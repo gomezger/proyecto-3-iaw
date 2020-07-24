d4fe90137b197402ccc2e137a068e971860d1e96
Xeron081495 <germang08@hotmail.com>
2020-07-24 20:17:25 -0300

    cambios en efecto

--- src/content/yes-login/puede/puede.js
+++ src/content/yes-login/puede/puede.js
@@ -22,22 +22,23 @@ export default class Puede extends React.Component{
 
 
     render(){
-        ScrollReveal().reveal('.titulo', { delay: 500,  easing: 'ease-in' });
-        ScrollReveal().reveal('.cargando', { delay: 700,  easing: 'ease-in'  });
+        ScrollReveal().reveal('.titulo-effect', { delay: 500,  easing: 'ease-in' });
+        ScrollReveal().reveal('.cargando-effect', { delay: 700,  easing: 'ease-in'  });
 
         var data;
         if(this.props.materias===null)
-            data = <div className="cargando col-12 mt-5 mb-5 float-left"><Cargando /></div>;    
+            data = <div className="cargando cargando-effect col-12 mt-5 mb-5 float-left"><Cargando /></div>;    
         else if(this.props.materias.length===0)
             data = <div className="col-12 mt-5 mb-5 float-left">No hay materias</div>;
         else{
-            data = <div className="animated fadeInRight col-12 p-0">{this.props.materias}</div>
+            ScrollReveal().reveal('.materias-effect', { delay: 700,  easing: 'ease-in'  });
+            data = <div className="animated materias-effect fadeInRight col-12 p-0">{this.props.materias}</div>
         }
 
         return (
             <div className="container">
                 <div className="col-12 p-0 float-left text-left">
-                    <h3 className="titulo col-12 p-0 float-left ">{this.props.titulo}</h3>
+                    <h3 className="titulo titulo-effect col-12 p-0 float-left ">{this.props.titulo}</h3>
                     {data}
                 </div>
             </div>
